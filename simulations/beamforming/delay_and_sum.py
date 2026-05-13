0db8ca37bfb57db57ae71a346e6427de903ff56c
JoachimNadal <joachim.nadal@protonmail.com>
2026-05-13 11:49:50 +0200

    Add DelayAndSumBeamformer class for beamforming
    Implemented a DelayAndSumBeamformer class to compute weights and beam patterns for an antenna array.

--- simulations/beamforming/delay_and_sum.py
+++ simulations/beamforming/delay_and_sum.py
@@ -1,1 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+
+class DelayAndSumBeamformer:
+
+    def __init__(self, antenna_array):
+        self.antenna_array = antenna_array
+
+    def compute_weights(self, theta_target, wavelength, unite="deg"):
+        a = self.antenna_array.compute_steering_vector(
+            theta_target,
+            wavelength,
+            unite
+        )
+
+        return a / self.antenna_array.num_elements
+
+    def compute_beam_pattern(self, theta_target, wavelength, unite="deg"):
+        weights = self.compute_weights(theta_target, wavelength, unite)
+
+        angles_deg = np.arange(-90, 91, 5)
+        beam_pattern = np.zeros(len(angles_deg), dtype=complex)
+
+        for idx, angle in enumerate(angles_deg):
+            a = self.antenna_array.compute_steering_vector(
+                angle,
+                wavelength,
+                "deg"
+            )
+
+            beam_pattern[idx] = np.vdot(weights, a)
+
+        gain = np.abs(beam_pattern) ** 2
+
+        plt.plot(angles_deg, gain)
+        plt.title("Delay-and-Sum Beam Pattern")
+        plt.xlabel("Angle (degrees)")
+        plt.ylabel("Normalized Gain")
+        plt.grid(True)
+        plt.show()
+
+        return angles_deg, gain
